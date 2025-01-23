4dc7fb94baad4a7db57d2de94a4157e24cf4f027
Angelo Pacelli <angelopacellipc@gmail.com>
2025-01-23 20:25:09 +0100

    aggiunte

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const video = document.getElementById("my-video");
 const playButton = document.getElementById("play-button");
 const startText = document.getElementById("start-text");
 
-playButton.addEventListener("click", () => {
+// Funzione per avviare o fermare il video
+function toggleVideo() {
   if (video.paused) {
     video.style.opacity = "1"; // Rendi visibile il video
     video.loop = true; // Abilita il loop
     video.play(); // Avvia il video
-    startText.style.display = "none"; // Nascondi la scritta quando il video parte
+    startText.style.display = "none"; // Nascondi la scritta
   } else {
     video.pause(); // Metti in pausa il video
     video.style.opacity = "0"; // Oscura di nuovo il video
-    startText.style.display = "block"; // Mostra la scritta per avviare il video
+    startText.style.display = "block"; // Mostra la scritta per avviare
   }
-});
+}
+
+// Aggiungi l'evento di clic a entrambi
+playButton.addEventListener("click", toggleVideo);
+startText.addEventListener("click", toggleVideo);
